bb429ff9d5645f255c5348d63dfda924c1a0754a
mdnaeemmiah <153842332+mdnaeemmiah@users.noreply.github.com>
2026-05-09 12:20:32 +0600

    first commit

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,7 @@ async function main() {
     await mongoose.connect(config.database_url as string);
 
     server = app.listen(port, () => {
-      console.log(`Mern-task app listening on port ${port}`);
+      console.log(`Motor-bridge app listening on port ${port}`);
     });
   } catch (err) {
     console.log(err);
